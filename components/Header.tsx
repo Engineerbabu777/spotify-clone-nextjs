a5f500c8f0d428fb967b1316916ea2177deb2673
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-29 23:52:58 +0500

    add: auth-completion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,39 +1,61 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import React from "react";
-import { BiSearch } from "react-icons/bi";
-import { FaUserAlt } from "react-icons/fa";
-import { HiHome } from "react-icons/hi";
-import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { useRouter } from "next/navigation";
+import { FaUserAlt } from "react-icons/fa";
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { toast } from "react-hot-toast";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+// import usePlayer from "@/hooks/usePlayer";
+
 import Button from "./Button";
 
 interface HeaderProps {
-	children: React.ReactNode;
-	className?: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ children, className }) => {
-	const router = useRouter();
+const Header: React.FC<HeaderProps> = ({
+  children,
+  className,
+}) => {
+  const player = usePlayer();
+  const router = useRouter();
+  const authModal = useAuthModal();
 
-	return (
-		<header
-			className={twMerge(
-				`
-          h-fit 
-          bg-gradient-to-b 
-          from-emerald-800 
-          p-6
-          `,
-				className
-			)}
-		>
-			<div className="w-full mb-4 flex items-center justify-between">
-				<div className="hidden md:flex gap-x-2 items-center">
-					<button
-						onClick={() => router.back()}
-						className="
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    // player.reset();
+    router.refresh();
+
+    if (error) {
+      toast.error(error.message);
+    }
+  }
+
+  return (
+    <div
+      className={twMerge(`
+        h-fit 
+        bg-gradient-to-b 
+        from-emerald-800 
+        p-6
+        `,
+        className
+      )}>
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button 
+            onClick={() => router.back()} 
+            className="
               rounded-full 
               bg-black 
               flex 
@@ -43,12 +65,12 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
               hover:opacity-75 
               transition
             "
-					>
-						<RxCaretLeft className="text-white" size={35} />
-					</button>
-					<button
-						onClick={() => router.forward()}
-						className="
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button 
+            onClick={() => router.forward()} 
+            className="
               rounded-full 
               bg-black 
               flex 
@@ -58,14 +80,14 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
               hover:opacity-75 
               transition
             "
-					>
-						<RxCaretRight className="text-white" size={35} />
-					</button>
-				</div>
-				<div className="flex md:hidden gap-x-2 items-center">
-					<button
-						onClick={() => router.push("/")}
-						className="
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button 
+            onClick={() => router.push('/')} 
+            className="
               rounded-full 
               p-2 
               bg-white 
@@ -76,12 +98,12 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
               hover:opacity-75 
               transition
             "
-					>
-						<HiHome className="text-black" size={20} />
-					</button>
-					<button
-						onClick={() => router.push("/search")}
-						className="
+          >
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button 
+            onClick={() => router.push('/search')} 
+            className="
               rounded-full 
               p-2 
               bg-white 
@@ -92,15 +114,15 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
               hover:opacity-75 
               transition
             "
-					>
-						<BiSearch className="text-black" size={20} />
-					</button>
-				</div>
-                <div className="flex justify-between items-center gap-x-4">
-          {true ? (
+          >
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          {user ? (
             <div className="flex gap-x-4 items-center">
               <Button 
-                // onClick={handleLogout} 
+                onClick={handleLogout} 
                 className="bg-white px-6 py-2"
               >
                 Logout
@@ -116,7 +138,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <>
               <div>
                 <Button 
-                //   onClick={authModal.onOpen} 
+                  onClick={authModal.onOpen} 
                   className="
                     bg-transparent 
                     text-neutral-300 
@@ -128,7 +150,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
               </div>
               <div>
                 <Button 
-                //   onClick={authModal.onOpen} 
+                  onClick={authModal.onOpen} 
                   className="bg-white px-6 py-2"
                 >
                   Log in
@@ -137,11 +159,10 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             </>
           )}
         </div>
-			</div>
-        {children}
-
-		</header>
-	);
-};
+      </div>
+      {children}
+    </div>
+  );
+}
 
 export default Header;
