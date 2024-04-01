18d42e9040dd1fd561f5ec3e4f76343e505e9f20
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-01 20:04:33 +0500

    add: media-item, search input some

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,63 +1,65 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { AiOutlinePlus } from 'react-icons/ai'
-import { TbPlaylist } from 'react-icons/tb'
+import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { TbPlaylist } from "react-icons/tb";
 
 import useUploadModal from "@/hooks/useUploadModal";
-import { useUser } from '@/hooks/useUser';
+import { useUser } from "@/hooks/useUser";
 import useAuthModal from "@/hooks/useAuthModal";
-import { Song } from '@/types';
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
 type Props = {
-  songs: Song[]
-}
+	songs: Song[];
+};
 
-export default function Library ({
-  songs
-}: Props) {
-  
-  const { user, subscription } = useUser();
-  const uploadModal = useUploadModal();
-  const authModal = useAuthModal();
-    const onClick = () => {
-      if (!user) {
-        return authModal.onOpen();
-      }
-  
-      // if (!subscription) {
-      //   return subscribeModal.onOpen();
-      // }
+export default function Library({ songs }: Props) {
+	const { user, subscription } = useUser();
+	const uploadModal = useUploadModal();
+	const authModal = useAuthModal();
+	const onClick = () => {
+		if (!user) {
+			return authModal.onOpen();
+		}
 
-      alert("hello")
-  
-     uploadModal.onOpen();
-    }
-  
-    return (
-    <div className='flex flex-col'>
-      <div className='flex items-center justify-between px-5 pt-4'>
-        <div className='inline-flex items-center gap-x--2'>
-          <TbPlaylist className="text-neutral-400"
-          size={26} 
-          />
-          <p className='text-neutral-400 font-medium text-md'>Your Library</p>
-        </div>
-        <AiOutlinePlus 
-          onClick={onClick} 
-          size={20} 
-          className="
+		// if (!subscription) {
+		//   return subscribeModal.onOpen();
+		// }
+
+		alert("hello");
+
+		uploadModal.onOpen();
+	};
+
+	return (
+		<div className="flex flex-col">
+			<div className="flex items-center justify-between px-5 pt-4">
+				<div className="inline-flex items-center gap-x--2">
+					<TbPlaylist className="text-neutral-400" size={26} />
+					<p className="text-neutral-400 font-medium text-md">Your Library</p>
+				</div>
+				<AiOutlinePlus
+					onClick={onClick}
+					size={20}
+					className="
             text-neutral-400 
             cursor-pointer 
             hover:text-white 
             transition
           "
-        />
-      </div>
+				/>
+			</div>
 
-      <div className="flex flex-col gap-y-2 mt-4 px-3">
-        abc
-      </div>
-    </div>
-  )
+			<div className="flex flex-col gap-y-2 mt-4 px-3">
+				{songs.map((item) => (
+					<MediaItem
+						onClick={(id: string) => onPlay(id)}
+						key={item.id}
+						data={item}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
